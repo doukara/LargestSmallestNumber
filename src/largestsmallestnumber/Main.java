6a4ae00efc1239cf56c194191ee479aa4a84656d
man55 <doukara001@gmail.com>
2018-09-30 18:29:29 +0100

    adding loop(for) for print  array items

--- src/largestsmallestnumber/Main.java
+++ src/largestsmallestnumber/Main.java
@@ -5,22 +5,27 @@ public class Main {
 
    
     public static void main(String[] args) {
-        
-    int  number[] = new int[] {12,96,20,10,88,106,37};
     
-    int smallest = number[0];
-    int largest = number[0];
+     //inser 10 numebr on array   
+    int  numbers[] = new int[] {12,96,20,10,88,106,37};
+    
+    int smallest = numbers[0];
+    int largest = numbers[0];
     int i = 0;
-    while(i  < number.length)
+    while(i  < numbers.length)
     {       
-        if(number[i] > largest)
-            largest = number[i];
-        else if (number[i] < smallest)
-	    smallest = number[i];
+        if(numbers[i] > largest)
+            largest = numbers[i];
+        else if (numbers[i] < smallest)
+	    smallest = numbers[i];
 	 i++;		
      }
-		
-	System.out.println("Largest Number is : " + largest);
+    
+    System.out.print("The numbers on this array : ");
+    for(int val : numbers)         
+        System.out.print("\t" + val);
+    		
+	System.out.println("\n" + "Largest Number is : " + largest);
 	System.out.println("Smallest Number is : " + smallest);
     }
     
