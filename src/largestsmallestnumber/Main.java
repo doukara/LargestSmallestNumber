77e15b3a30fa7c40c830801170bfd6bbb89a1ece
man55 <doukara001@gmail.com>
2018-10-01 19:34:00 +0100

    i finished most improtant things on projest

--- src/largestsmallestnumber/Main.java
+++ src/largestsmallestnumber/Main.java
@@ -1,32 +1,46 @@
 
 package largestsmallestnumber;
 
+import java.util.Scanner;
+
 public class Main {
 
-   
+    private static int smallest , largest;
     public static void main(String[] args) {
-    
-     //inser 10 numebr on array   
-    int  numbers[] = new int[] {12,96,20,10,88,106,37};
-    
-    int smallest = numbers[0];
-    int largest = numbers[0];
-    int i = 0;
-    while(i  < numbers.length)
-    {       
-        if(numbers[i] > largest)
+        
+            Scanner sc = new Scanner(System.in);
+
+
+           System.out.print("Enter the length of array : " );
+           int item = sc.nextInt();
+           //Fixed the length of array
+           int numbers[] = new int[item];
+           
+           System.out.println("Add the numbers on array  one by one Please  : " );
+           // add the item on array from the console
+           for(int i=0;i<numbers.length;i++){   
+               System.out.print((i+1) + "# " );
+               numbers[i] = sc.nextInt(); 
+           }
+           System.out.print("List of numbers : " );
+           //print the array items 
+           for(int val : numbers)
+               System.out.print("\t " + val );
+           
+           smallest = numbers[0];
+           largest = numbers[0];
+           int i = 0;
+           //Test all the items(numbers) on array 'numbers' with loop(while) and fix largest and smallest numbers  
+           while(i  < numbers.length){
+               
+            if(numbers[i] > largest)
             largest = numbers[i];
-        else if (numbers[i] < smallest)
+             else if (numbers[i] < smallest)
 	    smallest = numbers[i];
-	 i++;		
-     }
-    
-    System.out.print("The numbers on this array : ");
-    for(int val : numbers)         
-        System.out.print("\t" + val);
-    		
-	System.out.println("\n" + "Largest Number is : " + largest);
-	System.out.println("Smallest Number is : " + smallest);
+            i++;		
+        }
+           System.out.println("\n" + "Largest Number is : " + largest);  // \n for print largest Number  on a new line 
+           System.out.println("Smallest Number is : " + smallest);                
     }
     
 }
